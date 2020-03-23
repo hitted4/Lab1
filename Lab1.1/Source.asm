f633692ee2eb6080a1f0dd8dd68f7a80b2aa2253
dit15155@office365.uop.gr <dit15155@office365.uop.gr>
2020-03-23 17:57:35 +0200

    Declare Variables and sym constants

--- Lab1.1/Source.asm
+++ Lab1.1/Source.asm
@@ -1,11 +1,14 @@
 title Lab 1: Evalutaing Simple Arithmetic Expressions
 INCLUDE Irvine32.inc
-
+	p EQU 8d
 .data
-
+	x SDWORD 16d
+	y SDWORD 154d
+	z SDWORD -17d
+	w SDWORD ?
 .code
 main PROC
-
+	
 	exit
 main ENDP
 END main
